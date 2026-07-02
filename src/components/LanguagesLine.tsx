bfd937248d16e273fc36149224894d8dfe8239d9
vadimk321 <8246vadim8246@mail.ru>
2026-07-02 09:13:03 +0300

    Add Languages Line

--- src/components/LanguagesLine.tsx
+++ src/components/LanguagesLine.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 type LanguageStat = {
   language: string;
@@ -14,7 +15,68 @@ type LanguagesLineProps = {
 
 
 export default function LanguagesLine({languages}: LanguagesLineProps) {
+    const languageColors: Record<string, string> = {
+        JavaScript: "#f1e05a",
+        TypeScript: "#3178c6",
+        HTML: "#e34c26",
+        CSS: "#563d7c",
 
+        Python: "#3572A5",
+        Java: "#b07219",
+        C: "#555555",
+        "C++": "#f34b7d",
+        "C#": "#178600",
+        Go: "#00ADD8",
+        Rust: "#dea584",
+        Ruby: "#701516",
+        PHP: "#4F5D95",
+        Swift: "#F05138",
+        Kotlin: "#A97BFF",
+        Dart: "#00B4AB",
+        Scala: "#c22d40",
+
+        Shell: "#89e051",
+        PowerShell: "#012456",
+        Batchfile: "#C1F12E",
+
+        Vue: "#41b883",
+        Svelte: "#ff3e00",
+        Astro: "#ff5a03",
+
+        SCSS: "#c6538c",
+        Sass: "#a53b70",
+        Less: "#1d365d",
+
+        JSON: "#292929",
+        YAML: "#cb171e",
+        XML: "#0060ac",
+        Markdown: "#083fa1",
+
+        Dockerfile: "#384d54",
+        CMake: "#DA3434",
+        Makefile: "#427819",
+
+        ObjectiveC: "#438eff",
+        "Objective-C": "#438eff",
+
+        Perl: "#0298c3",
+        Lua: "#000080",
+        R: "#198CE7",
+        Haskell: "#5e5086",
+        Elixir: "#6e4a7e",
+        Erlang: "#B83998",
+        Clojure: "#db5855",
+        FSharp: "#b845fc",
+        "F#": "#b845fc",
+
+        JupyterNotebook: "#DA5B0B",
+        "Jupyter Notebook": "#DA5B0B",
+
+        TeX: "#3D6117",
+        VimScript: "#199f4b",
+        "Vim Script": "#199f4b",
+    };
+    
     const percentages = calculate(languages);
 
     useEffect(() => {
@@ -42,15 +104,14 @@ export default function LanguagesLine({languages}: LanguagesLineProps) {
 
     return (
         <Box>
-            <div className="languages-line">
+            <div className="languages-line languages-line-pos">
                 {percentages.map(lang => (
                     <div
                         key={lang.language}
                         className={lang.language}
                         style={{
                             width: `${lang.percent}%`
-                        }}
-                        >
+                        }}>
                     </div>
                 ))}
             </div>
@@ -58,10 +119,13 @@ export default function LanguagesLine({languages}: LanguagesLineProps) {
                 <Box sx={{
                     display: "flex",
                     alignItems: "center",
-                    gap: 1
-                }}>
-                    <div className={`${lang.language}-circle`}></div>
-                    <p>{lang.language}: {lang.percent}%</p>
+                    gap: 1}}>
+                    <span
+                        className="circle"
+                        style={{
+                            "--language-color": languageColors[lang.language] ?? "#8b949e",
+                        } as React.CSSProperties}/>
+                    <Typography>{lang.language}: {lang.percent}%</Typography>
                 </Box>
             ))}
         </Box>
--- src/components/UserRepository.tsx
+++ src/components/UserRepository.tsx
@@ -42,9 +42,24 @@ function UserRepository({rep} : UserRepositoryProps) {
             
           </Typography>
 
-          <Typography color="text.secondary">
-            ID: {rep.id}
-          </Typography>
+          <Box sx={{display: "flex", gap: 2}}>
+            <Typography color="text.secondary">
+              ID: {rep.id}
+            </Typography>
+            <Typography sx={{display: "flex", alignItems: "center", gap: 1}}>
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                aria-hidden="true"
+              >
+                <path
+                  fill="#eac54f"
+                  d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.969.719 4.193a.75.75 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Z"
+                />
+              </svg> {rep.stargazers_count}
+            </Typography>
+          </Box>
 
           {rep.description ? 
             <Typography sx={{
@@ -60,9 +75,7 @@ function UserRepository({rep} : UserRepositoryProps) {
             flexDirection: "column", 
             gap: 2, 
             mt: 1 }}>
-            <Typography>
-              Stars: {rep.stargazers_count}
-            </Typography>
+            
             <Box>
               {rep.languages ? <LanguagesLine 
                 languages={rep.languages}
